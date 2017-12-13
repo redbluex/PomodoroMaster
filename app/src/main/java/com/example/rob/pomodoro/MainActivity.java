e2d9556885d738e205afe947d6f0e464e2bc2036
RobertW1995 <robert.wasiukiewicz@gmail.com>
2017-12-13 19:34:14 +0100

    First init

---
+++ app/src/main/java/com/example/rob/pomodoro/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.rob.pomodoro;
+
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.sql.Time;
+import java.util.Timer;
+
+public class MainActivity extends AppCompatActivity {
+
+    public int counter = 0;
+    String tekstRamka;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button click = (Button)findViewById(R.id.button);
+        Button clickset = (Button)findViewById(R.id.buttonAccept);
+        final TextView label = (TextView)findViewById(R.id.timeText);
+
+        clickset.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                EditText editRamka = (EditText)findViewById(R.id.setTime);
+                tekstRamka = editRamka.getText().toString();
+                label.setText(tekstRamka);
+            }
+        });
+
+        click.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                new CountDownTimer(10000, 1000){
+                    public void onTick(long millisOnFinished){
+                        label.setText(String.valueOf(Integer.parseInt(tekstRamka)-counter));
+                        counter++;
+                    }
+
+                    @Override
+                    public void onFinish() {
+                        label.setText("Koniec czasu");
+                    }
+                }.start();
+            }
+        });
+
+    }
+
+
+
+
+
+
+}
