b9c63a2b0ac5d2c6bd5b7727a09d02ad8040f6c0
redbluex <redbluedev@gmail.com>
2017-12-23 14:43:14 +0100

    new colors and font

--- app/src/main/java/com/example/rob/pomodoro/Settings.java
+++ app/src/main/java/com/example/rob/pomodoro/Settings.java
@@ -1,20 +1,40 @@
 package com.example.rob.pomodoro;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import static com.example.rob.pomodoro.MainActivity.ustawienia;
 
 public class Settings extends AppCompatActivity {
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        EditText a = (EditText)findViewById(R.id.editTextMT);
+        setFont(a);
+        EditText b = (EditText)findViewById(R.id.editTextSB);
+        setFont(b);
+        EditText c = (EditText)findViewById(R.id.editTextLB);
+        setFont(c);
+        TextView x = (TextView)findViewById(R.id.textMT);
+        setFont(x);
+        TextView y = (TextView)findViewById(R.id.textSB);
+        setFont(y);
+        TextView z = (TextView)findViewById(R.id.textLB);
+        setFont(z);
+        Button k = (Button)findViewById(R.id.buttonSave);
+        setFont(k);
+
+
     }
 
 
@@ -32,4 +52,17 @@ public class Settings extends AppCompatActivity {
         Intent i = new Intent(Settings.this, MainActivity.class);
         startActivity(i);
     }
+    private void setFont(EditText myTextView){
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/Munro.ttf");
+        myTextView.setTypeface(typeface);
+    }
+    private void setFont(TextView myTextView){
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/Munro.ttf");
+        myTextView.setTypeface(typeface);
+    }
+    private void setFont(Button myTextView){
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/Munro.ttf");
+        myTextView.setTypeface(typeface);
+    }
+
 }
